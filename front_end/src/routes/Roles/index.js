3266fb6406dcbf71e9a132cb997480199d8ad5d2
Zac Messinger <zdmessinger@gmail.com>
2023-06-13 18:53:51 -0700

    fixed routes page

--- front_end/src/routes/Roles/index.js
+++ front_end/src/routes/Roles/index.js
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from "react";
 import { useLoaderData, useNavigation } from "react-router-dom";
 
 export default function Roles() {
-	const { companies } = useLoaderData();
 	const navigation = useNavigation();
 
 	return <h1>This is the open roles page!</h1>;
