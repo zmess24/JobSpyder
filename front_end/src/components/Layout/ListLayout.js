04d75d3bb273bc246a4a7979261ec2c87e7b5a89
Zac Messinger <zdmessinger@gmail.com>
2023-06-21 11:59:10 -0700

    filtering correctly now

--- front_end/src/components/Layout/ListLayout.js
+++ front_end/src/components/Layout/ListLayout.js
@@ -24,31 +24,8 @@ export default function ListLayout({ data, ListItemComponent, layoutCSS, filters
 	const [scroll, setScroll] = useState(true);
 	const [dataToRender, setDataToRender] = useState(data.slice(0, INFINITE_SCROLL_STEP));
 
-	// Handle Filter Change
-	const addFilter = (value, type) => {
-		setActiveIndustry([...activeIndustry, { value, label: value, type }]);
-	};
-
-	const removeFilter = (value, type) => {
-		setActiveIndustry(activeIndustry.filter((a) => a.value !== value));
-	};
-
-	// Filter State Varialbes
-	const loadData = () => {
-		setIndex(index + INFINITE_SCROLL_STEP);
-		let dataToRenderList = allData.slice(0, index + INFINITE_SCROLL_STEP);
-		setDataToRender(dataToRenderList);
-		if (dataToRenderList.length === allData.length) setScroll(false);
-	};
-
-	const loadSearchResults = () => {
-		setSearchIndex(searchIndex + INFINITE_SCROLL_STEP);
-		let dataToRenderList = searchResults.slice(0, searchIndex + INFINITE_SCROLL_STEP);
-		setDataToRender(dataToRenderList);
-		if (dataToRenderList.length === searchResults.length) setSearchScroll(false);
-	};
-
-	const filterResults = (searchItem, keyToSearch) => {
+	// Filter Results Function
+	const filterResults = ({ searchCriteria, keyToSearch }) => {
 		let sliceIntoChunks = (arr, chunkSize) => {
 			const res = [];
 			for (let i = 0; i < arr.length; i += chunkSize) {
@@ -63,19 +40,58 @@ export default function ListLayout({ data, ListItemComponent, layoutCSS, filters
 
 		arrays.forEach((array) => {
 			let results = array.filter((data) => {
-				let formatted_role = data[keyToSearch].toLowerCase().replace(" ", "");
-				return formatted_role.indexOf(searchItem) > -1;
+				debugger;
+				if (keyToSearch === "industries" && searchCriteria.length > 0) {
+					let found = searchCriteria.find(({ value }) => {
+						return data[keyToSearch].indexOf(value) > -1;
+					});
+					return found;
+				} else {
+					let formatted_role = data[keyToSearch].toLowerCase().replace(" ", "");
+					return formatted_role.indexOf(searchCriteria) > -1;
+				}
 			});
 
 			arrayRef = arrayRef.concat(results);
 		});
 
-		if (arrayRef.length < INFINITE_SCROLL_STEP) {
-			setSearchScroll(false);
-		}
-
+		if (arrayRef.length < INFINITE_SCROLL_STEP) setSearchScroll(false);
 		setDataToRender(arrayRef.slice(0, INFINITE_SCROLL_STEP));
 		setSearchResults(arrayRef);
+	};
+
+	// Handle Filter Change
+	const addFilter = (value, type) => {
+		let industries = [...activeIndustry, { value, label: value, type }];
+		setActiveIndustry(industries);
+		filterResults({ searchCriteria: industries, keyToSearch: "industries" });
+	};
+
+	const removeFilter = (value, type) => {
+		let industries = activeIndustry.filter((a) => a.value !== value);
+		setActiveIndustry(industries);
+		if (industries.length === 0 && searchTerm === "") {
+			setSearchResults([]);
+			setSearchIndex(INFINITE_SCROLL_STEP);
+			setDataToRender(allData.slice(0, index));
+		} else {
+			filterResults({ searchCriteria: industries, keyToSearch: "industries" });
+		}
+	};
+
+	// Filter State Varialbes
+	const loadData = () => {
+		if (searchTerm || activeIndustry.length > 0) {
+			setSearchIndex(searchIndex + INFINITE_SCROLL_STEP);
+			let dataToRenderList = searchResults.slice(0, searchIndex + INFINITE_SCROLL_STEP);
+			setDataToRender(dataToRenderList);
+			if (dataToRenderList.length === searchResults.length) setSearchScroll(false);
+		} else {
+			setIndex(index + INFINITE_SCROLL_STEP);
+			let dataToRenderList = allData.slice(0, index + INFINITE_SCROLL_STEP);
+			setDataToRender(dataToRenderList);
+			if (dataToRenderList.length === allData.length) setScroll(false);
+		}
 	};
 
 	const handleSearchTermChange = (e) => {
@@ -85,16 +101,15 @@ export default function ListLayout({ data, ListItemComponent, layoutCSS, filters
 		if (e.target.value == "") {
 			setSearchResults([]);
 			setSearchIndex(INFINITE_SCROLL_STEP);
-			setDataToRender(data.slice(0, index));
+			setDataToRender(allData.slice(0, index));
 		} else {
 			let formatted_search = e.target.value.toLowerCase().replace(" ", "");
-			filterResults(formatted_search, "title");
+			filterResults({ searchCriteria: formatted_search, keyToSearch: "title" });
 		}
 	};
 
-	let nextLoader = searchTerm ? loadSearchResults : loadData;
-	let scrollState = searchTerm ? searchScroll : scroll;
-	let resultsTotal = searchTerm ? searchResults.length : allData.length;
+	let scrollState = searchTerm || activeIndustry.length > 0 ? searchScroll : scroll;
+	let resultsTotal = searchTerm || activeIndustry.length > 0 ? searchResults.length : allData.length;
 
 	return (
 		<>
@@ -103,7 +118,7 @@ export default function ListLayout({ data, ListItemComponent, layoutCSS, filters
 				<Results total={resultsTotal.toLocaleString("en-US")} />
 				<ActiveFilters activeOptions={activeIndustry} removeFilter={removeFilter} />
 			</div>
-			<InfiniteScroll dataLength={dataToRender.length} next={nextLoader} hasMore={scrollState} loader={<p>Loading...</p>}>
+			<InfiniteScroll dataLength={dataToRender.length} next={loadData} hasMore={scrollState} loader={<p>Loading...</p>}>
 				<ul role="list" className={layoutCSS}>
 					{dataToRender &&
 						dataToRender.map((role) => {
