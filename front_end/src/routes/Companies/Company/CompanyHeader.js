f7e92af79bc893e043204d8dd7a19dd95196ccfd
Zac Messinger <zdmessinger@gmail.com>
2023-06-26 16:16:45 -0700

    added rel=noreferrer to a tag

--- front_end/src/routes/Companies/Company/CompanyHeader.js
+++ front_end/src/routes/Companies/Company/CompanyHeader.js
@@ -35,6 +35,7 @@ export default function CompanyHeader({ company }) {
 						<a
 							href={company.company_link}
 							target="_blank"
+							rel="noreferrer"
 							className="flex items-center justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
 						>
 							Company Website
