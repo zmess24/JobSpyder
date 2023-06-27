cf720c1fb9266024d60e837f70e794f44865bd62
Zac Messinger <zdmessinger@gmail.com>
2023-06-26 19:13:21 -0700

    updated

--- front_end/src/components/Navbar/index.js
+++ front_end/src/components/Navbar/index.js
@@ -2,6 +2,7 @@ import { Disclosure } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { NavLink } from "react-router-dom";
 import MobileMenu from "./MobileMenu";
+import logo from "../../assets/logo.png";
 
 export default function Navbar() {
 	let ActiveLink = "inline-flex items-center border-b-2 border-indigo-500 px-1 pt-1 text-sm font-medium text-gray-900";
@@ -15,8 +16,8 @@ export default function Navbar() {
 						<div className="flex h-16 justify-between">
 							<div className="flex">
 								<div className="flex flex-shrink-0 items-center">
-									<img className="block h-12 w-auto lg:hidden" src="./js-logo.png" alt="Your Company" />
-									<img className="hidden h-12 w-auto lg:block" src="./js-logo.png" alt="Your Company" />
+									<img className="block h-12 w-auto lg:hidden" src={logo} alt="Your Company" />
+									<img className="hidden h-12 w-auto lg:block" src={logo} alt="Your Company" />
 								</div>
 								<div className="hidden sm:ml-6 sm:flex sm:space-x-8">
 									<NavLink
