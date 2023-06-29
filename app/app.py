5aedbbc8ecb6166b1c9027a05c514cf8dc6ac1f9
Zac Messinger <zdmessinger@gmail.com>
2023-06-29 11:56:50 -0700

    updated

--- app/app.py
+++ app/app.py
@@ -13,9 +13,9 @@ if __name__ == "__main__":
     application.run()
 
 if __name__ == "app":
-    from waitress import serve
+    # from waitress import serve
     application = create_app()
     application.config['MONGO_URI'] = os.getenv('MONGODB_URI_DEV')
     application.config['MONGO_URI'] = os.getenv('MONGODB_URI_DEV')
-    # application.run()
-    serve(application, host="0.0.0.0", port=8080)
+    application.run()
+    # serve(application, host="0.0.0.0", port=8080)
