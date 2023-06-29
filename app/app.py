e908c4710d43218f614369498f070a86f2110744
Zac Messinger <zdmessinger@gmail.com>
2023-06-29 12:26:54 -0700

    heroku

--- app/app.py
+++ app/app.py
@@ -12,10 +12,9 @@ if __name__ == "__main__":
     application.config['MONGO_URI'] = os.getenv('MONGODB_URI_DEV')
     application.run()
 
-# if __name__ == "app":
-#     # from waitress import serve
-#     application = create_app()
-#     application.config['MONGO_URI'] = os.getenv('MONGODB_URI_DEV')
-#     application.config['MONGO_URI'] = os.getenv('MONGODB_URI_DEV')
-#     application.run()
-#     # serve(application, host="0.0.0.0", port=8080)
+if __name__ == "app":
+    # from waitress import serve
+    application = create_app()
+    application.config['MONGO_URI'] = os.getenv('MONGODB_URI_DEV')
+    application.run()
+    # serve(application, host="0.0.0.0", port=8080)
