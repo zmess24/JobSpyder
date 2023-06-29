aad3887e3c91c10fb0cdcabfcb1319589a40d595
Zac Messinger <zdmessinger@gmail.com>
2023-06-29 12:25:02 -0700

    heroku

--- app/app.py
+++ app/app.py
@@ -6,9 +6,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 if __name__ == "__main__":
+    print("ENTERING HEROKU!")
     application = create_app()
     application.config['DEBUG'] = True
-    application.config['MONGO_URI'] = os.getenv('MONGODB_URI_DEV')
     application.config['MONGO_URI'] = os.getenv('MONGODB_URI_DEV')
     application.run()
 
